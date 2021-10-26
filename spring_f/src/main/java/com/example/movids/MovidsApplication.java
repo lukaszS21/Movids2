6ce8c92dbe5dd8c8d28e778a9880f4755c6fa6cf
Łukasz Stolarz <lukaszstolarz@op.pl>
2021-10-26 11:16:23 +0200

    spring init

---
+++ spring_f/src/main/java/com/example/movids/MovidsApplication.java
@@ -0,0 +1,13 @@
+package com.example.movids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovidsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovidsApplication.class, args);
+    }
+
+}
---
+++ spring_f/src/test/java/com/example/movids/MovidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovidsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
